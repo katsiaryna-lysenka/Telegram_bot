918f0599b995ca3b343b9e49948c8d522bfb41d5
katsiaryna-lysenka <katya.lysenko.1999@gmail.com>
2024-01-23 23:10:15 +0300

    Add new process_name

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ import logging
 import sys
 import asyncio
 import csv
-from contextlib import contextmanager
+from contextlib import contextmanager, asynccontextmanager
 
 from aiogram import Bot, Dispatcher, types, Router, html
+from aiogram.client import bot
 from aiogram.filters import Command, CommandStart
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 import aiohttp
 from os import getenv
 from aiogram.enums import ParseMode
-from sqlalchemy import create_engine, Column, Integer, String, Sequence
+from aiogram.fsm.storage.memory import MemoryStorage
+from sqlalchemy import create_engine, Column, Integer, String, Sequence, select
 from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from bs4 import BeautifulSoup
@@ -24,13 +26,11 @@ from aiogram.types import (
 
 # Установите свой токен бота
 TOKEN = "6597211233:AAGstMTkO1le_rD1JFlfX6kPF-4jlDlxMLg"
-
 DATABASE_URL = 'sqlite+aiosqlite:///site_parser.db'
 engine = create_async_engine(DATABASE_URL, echo=False, future=True)
 Base = declarative_base()
 
 
-# Определение модели данных для пользователя
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
@@ -38,7 +38,6 @@ class User(Base):
     state = Column(String(50), nullable=True)
 
 
-# Определение модели данных для товара
 class Product(Base):
     __tablename__ = 'products'
     id = Column(Integer, Sequence('product_id_seq'), primary_key=True)
@@ -48,19 +47,26 @@ class Product(Base):
     availability = Column(String(50), nullable=False)
     info = Column(String(255), nullable=False)
 
+
 async def create_tables():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
 
+class UserState(StatesGroup):
+    waiting_for_name = State()
+    waiting_for_category = State()
+    waiting_the_answer = State()
+
+
 form_router = Router()
+storage = MemoryStorage()
+dp = Dispatcher()
 
 
-#Session = sessionmaker(bind=engine)
-
-@contextmanager
+@asynccontextmanager
 async def get_session():
-    async_session = AsyncSession()
+    async_session = AsyncSession(bind=engine)
     try:
         yield async_session
         await async_session.commit()
@@ -69,14 +75,6 @@ async def get_session():
         raise
     finally:
         await async_session.close()
-
-
-
-# Функционал состояний для FSM
-class UserState(StatesGroup):
-    waiting_for_name = State()
-    waiting_for_category = State()
-    waiting_my_answer = State()
 
 
 # Обработчик команды /start
@@ -89,9 +87,6 @@ async def cmd_start(message: Message, state: FSMContext):
     )
 
 
-# Обработчик ввода имени пользователя
-# ...
-
 @form_router.message(UserState.waiting_for_name)
 async def process_name(message: Message, state: FSMContext):
     await state.set_state(UserState.waiting_for_category)
@@ -99,28 +94,31 @@ async def process_name(message: Message, state: FSMContext):
     user = User(username=user_name)
     session = sessionmaker(bind=engine)()
     session.add(user)
-    #await session.commit()
     await message.answer(
         f"Привет, {user_name}! Теперь вы можете ввести ссылку на категорию товара.",
         reply_markup=ReplyKeyboardRemove(),
     )
 
-
-# Обработчик ввода ссылки на категорию
-@form_router.message(UserState.waiting_for_category)
+@form_router.message()
 async def process_category(message: Message, state: FSMContext):
+    print(f'1')
     user_id = message.from_user.id
+    print(f"2")
 
     try:
-        # Используйте контекстный менеджер для работы с сессией
+        # Используйте async with для работы с асинхронной сессией
         async with get_session() as user_session:
-            user = await user_session.query(User).filter(User.id == user_id).first()
+            # Используем select для асинхронного запроса
+            stmt = select(User).where(User.id == user_id)
+            result = await user_session.execute(stmt)
+            user = result.scalar()
+
             print(f"user: {user}")
 
             if user:
                 category_link = message.text
                 await message.answer("Пожалуйста, ожидайте завершения парсинга. Это может занять некоторое время.")
-                await parse_category(user_id, category_link)
+                await parse_category(user.id, category_link)
                 await message.answer("Парсинг завершен. Результаты сохранены в файле products.csv.")
             else:
                 print(f"User with id {user_id} not found or in an invalid state.")
@@ -129,15 +127,7 @@ async def process_category(message: Message, state: FSMContext):
         print(f"Exception: {e}")
 
 
-# Обработчик команды /help
-# @form_router.message(CommandHelp())
-# async def cmd_help(message: Message, state: FSMContext):
-#     await message.answer("Доступные команды:\n"
-#                          "/start - начать работу\n"
-#                          "/help - получить справку")
 
-
-# Функция парсинга категории
 async def parse_category(user_id, category_link):
     try:
         async with aiohttp.ClientSession() as session:
@@ -159,7 +149,8 @@ async def parse_category(user_id, category_link):
                     availability = product_soup.find('p', class_='instock availability').text.strip()
                     info = product_soup.find('meta', {'name': 'description'})['content']
 
-                    product = Product(name=product_name, price=price, link=product_link, availability=availability, info=info)
+                    product = Product(name=product_name, price=price, link=product_link, availability=availability,
+                                      info=info)
                     products.append(product)
 
         await save_to_csv(products)
@@ -167,15 +158,13 @@ async def parse_category(user_id, category_link):
     except Exception as e:
         print(f"Error during parsing: {e}")
 
-
-
-# Функция сохранения результатов в CSV
 async def save_to_csv(products):
     with open('products.csv', 'w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
         writer.writerow(['Название', 'Цена', 'Ссылка', 'Наличие', 'Информация'])
         for product in products:
             writer.writerow([product.name, product.price, product.link, product.availability, product.info])
+
 
 async def main():
     bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
@@ -184,8 +173,10 @@ async def main():
 
     await dp.start_polling(bot)
 
-# Запуск бота
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
+
+
 
